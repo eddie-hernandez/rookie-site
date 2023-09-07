7a3a47bb9ace423a7d196c01cd71bdbc92571d0b
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-09-07 13:44:44 -0400

    adding Gsheets api fetch for remote maintanence

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Content.css'
 import RookieVideo from '../../assets/rookiecommercial2023.mov'
 import ReactPlayer from 'react-player'
 import OrderButton from '../../assets/order-button-no-border.png'
 
-export default function Content({ videoEnded, showModal, playerRef, setVideoEnded }) {
+export default function Content({
+  videoEnded,
+  showModal,
+  playerRef,
+  setVideoEnded,
+}) {
+  const [resLink, setResLink] = useState('')
+
+  const spreadsheetId = process.env.REACT_APP_GOOGLE_SPREADSHEET_ID
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
+  const apiURL = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/sheet1!C2?key=${apiKey}`
+
+  useEffect(() => {
+    // fetch data from Google Sheets
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((data) => {
+        setResLink(data.values[0][0])
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+      })
+  }, [])
 
   return (
     <div className={videoEnded ? 'button-container' : 'video-container'}>
@@ -13,24 +35,29 @@ export default function Content({ videoEnded, showModal, playerRef, setVideoEnde
           ref={playerRef}
           url={RookieVideo}
           playing={!showModal}
-          width='100%'
-          height='100%'
-          className='react-player'
-          onEnded={() => {setVideoEnded(true)}}
+          width="100%"
+          height="100%"
+          className="react-player"
+          onEnded={() => {
+            setVideoEnded(true)
+          }}
           controls={false}
           playsinline
         />
       ) : (
-        <button
-          className='rookie-button'
+        <a
+          href={resLink}
+          rel="noreferrer"
+          target="_blank"
+          className="rookie-button"
           style={{ display: videoEnded ? 'block' : 'none' }}
         >
           <img
-            className='rookie-button-image'
+            className="rookie-button-image"
             src={OrderButton}
-            alt='Rookie Button'
+            alt="Rookie Button"
           />
-        </button>
+        </a>
       )}
     </div>
   )
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -2,38 +2,63 @@ import React, { useEffect, useState } from 'react'
 import './Intro.css'
 
 export default function Intro({ showModal, setShowModal, handleEnter }) {
-  const [headerContent, setHeaderContent] = useState('')
-  const [postContent, setPostContent] = useState('')
+  const [aContent, setAContent] = useState(['', '', '']);
+  const [bContent, setBContent] = useState(['', '', '']);
 
   const spreadsheetId = process.env.REACT_APP_GOOGLE_SPREADSHEET_ID
   const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
-  const apiURL = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/sheet1!A2:B2?key=${apiKey}`
+  const apiURL = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/sheet1!A2:B4?key=${apiKey}`
 
   useEffect(() => {
     // Fetch data from Google Sheets
     fetch(apiURL)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.values[0])
-        setHeaderContent(data.values[0][0])
-        setPostContent(data.values[0][1])
+        data.values.forEach((row, rowIndex) => {
+          row.forEach((cell, columnIndex) => {
+            const value = cell || ''; // Set to empty string if undefined
+            if (columnIndex === 0) {
+              setAContent((prevAContent) => {
+                const newAContent = [...prevAContent];
+                newAContent[rowIndex] = value;
+                return newAContent;
+              });
+            } else if (columnIndex === 1) {
+              setBContent((prevBContent) => {
+                const newBContent = [...prevBContent];
+                newBContent[rowIndex] = value;
+                return newBContent;
+              });
+            }
+          });
+        });
       })
       .catch((error) => {
-        console.error('Error fetching data:', error)
-      })
-  }, [])
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
     <div className={`modal ${showModal ? 'show' : ''}`}>
       <div className="modal-content">
+        <div className='scrolling-text-container'>
+          <div className='scrolling-text'>
+            ROOKIE PIZZA
+          </div>
+
+        </div>
         <h1>WELCOME TO ROOKIE PIZZA</h1>
         <h3>
           This website includes sound.
           <br />
           We recommend turning it on for the full experience.
         </h3>
-        <h3>{headerContent}</h3>
-        <p>{postContent}</p>
+        {aContent[0] ? <h3>{aContent[0]}</h3> : ''}
+        {aContent[1] ? <h3>{aContent[1]}</h3> : ''}
+        {aContent[2] ? <h3>{aContent[2]}</h3> : ''}
+        {bContent[0] ? <p>{bContent[0]}</p> : ''}
+        {bContent[1] ? <p>{bContent[1]}</p> : ''}
+        {bContent[2] ? <p>{bContent[2]}</p> : ''}
         <br />
         <button
           onClick={() => {
